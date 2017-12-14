310f03838fee396ddfaa0a93c93b1a8d4304ba7b
starkadder <tschweiger@apio.tech>
2017-12-14 12:49:54 -0800

    new version of akka http

--- src/main/g8/project/Versions.scala
+++ src/main/g8/project/Versions.scala
@@ -1,6 +1,6 @@
 object Versions {
   val akkaV = "2.5.4"
-  val akkaHttpV = "10.0.10"
+  val akkaHttpV = "10.0.11"
   val squbsV = "0.10.0-SNAPSHOT"
   val logbackClassicV = "1.1.3"
   val json4sV = "3.5.0"
